29eba91daf4d165812a7e070c52042793a19ca2d
GualaBanana <tem4rik@gmail.com>
2021-11-05 18:20:20 +0300

    Add a simple installer.

---
+++ Installer/Installer.cs
@@ -0,0 +1,27 @@
+﻿BackpUk.Installer.Installer.Run();
+
+namespace BackpUk.Installer
+{
+    internal static class Installer
+    {
+        public static void Run()
+        {
+            if (IsAlreadyInstalled)
+            {
+                Console.WriteLine($"{Config.AppName} is already installed.");
+                return;
+            }
+
+            InitializeLocalAppDataDirectory();
+            Console.WriteLine($"{Config.AppName} has been successfully installed.");
+        }
+
+        static bool IsAlreadyInstalled => Directory.Exists(Config.LocalAppData);
+        static void InitializeLocalAppDataDirectory()
+        {
+            Directory.CreateDirectory(Config.LocalAppData);
+            File.Create(Config.TrackerFileLocation);
+            File.Create(Config.UserSettingsFileLocation);
+        }
+    }
+}
